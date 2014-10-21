1032c91a4c5977db14d937cff632f2325b6e8a10
jayaregalinada <jayaregalinada@gmail.com>
2014-10-22 00:42:52 +0800

    Make it modular

--- src/Walisph/Dashboard/DashboardServiceProvider.php
+++ src/Walisph/Dashboard/DashboardServiceProvider.php
@@ -1,7 +1,9 @@
 <?php namespace Walisph\Dashboard;
 
 use Illuminate\Support\ServiceProvider;
+use Intervention\Image\ImageManager;
 use Walisph\Dashboard\Repositories\DashboardRepository;
+use Walisph\Dashboard\Repositories\UploadRepository;
 
 class DashboardServiceProvider extends ServiceProvider {
 
@@ -29,8 +31,28 @@ class DashboardServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		$this->app->bind('\Walisph\Dashboard\Repositories\DashboardRepositoryInterface', function( $app ){
-			return new DashboardRepository( $app['file'], $app['config'] );
+		foreach([
+			'Upload',
+			'Dashboard'] as $module)
+		{
+			$this->{"register$module"}();
+		}
+		
+	}
+	
+	protected function registerUpload()
+	{
+		$this->app->bind('\Walisph\Dashboard\Repositories\UploadRepositoryInterface', function( $app )
+		{
+			return new UploadRepository( new ImageManager( $app['config']->get('walis-dashboard::image') ));
+		});
+	}
+	
+	protected function registerDashboard()
+	{
+		$this->app->bind('\Walisph\Dashboard\Repositories\DashboardRepositoryInterface', function( $app )
+		{
+			return new DashboardRepository( $app['files'], $app['config'] );
 		});
 	}
 
