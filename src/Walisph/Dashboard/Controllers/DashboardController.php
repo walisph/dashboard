821709cd522b244e38e8fdf6c890f2b775571958
jayaregalinada <jayaregalinada@gmail.com>
2014-10-22 00:42:39 +0800

    Fixed indentation

--- src/Walisph/Dashboard/Controllers/DashboardController.php
+++ src/Walisph/Dashboard/Controllers/DashboardController.php
@@ -2,17 +2,80 @@
 
 use BaseController;
 use Walisph\Dashboard\Repositories\DashboardRepositoryInterface;
+use Walisph\Dashboard\Repositories\UploadRepositoryInterface;
 
 class DashboardController extends BaseController {
 
-    /**
-     * @var DashboardRepositoryInterface
-     */
-    protected $dash;
+	/**
+	 * @var DashboardRepositoryInterface
+	 */
+	protected $dash;
 
-    public function __construct( DashboardRepositoryInterface $dash )
-    {
-        $this->dash = $dash;
-    }
+	/**
+	 * @var UploadRepositoryInterface
+	 */
+	protected $upload;
+
+	/**
+	 * @param DashboardRepositoryInterface $dash
+	 * @param UploadRepositoryInterface $upload
+	 */
+	public function __construct( DashboardRepositoryInterface $dash, UploadRepositoryInterface $upload )
+	{
+		$this->dash     = $dash;
+		$this->upload   = $upload;
+	}
+
+	public function getConfiguration( $config = 'config', $name = null )
+	{
+		if ( ! $name )
+		{
+			return $this->dash->getConfig()->get( 'walis.dashboard::' . $config );
+		}
+
+		return $this->dash->getConfig()->get( 'walis-dashboard::' . $config . '.' . $name );
+	}
+
+	/**
+	 * @return DashboardRepositoryInterface
+	 */
+	public function getDash()
+	{
+		return $this->dash;
+	}
+
+	/**
+	 * @return DashboardRepositoryInterface
+	 */
+	public function getDashboardRepository()
+	{
+		return $this->dash;
+	}
+
+	/**
+	 * @return UploadRepositoryInterface
+	 */
+	public function getUpload()
+	{
+		return $this->upload;
+	}
+
+	/**
+	 * @return UploadRepositoryInterface
+	 */
+	public function getUploadRepository()
+	{
+		return $this->upload;
+	}
+
+	/**
+	 * Get instance of Image of Intervention
+	 *
+	 * @return mixed
+	 */
+	protected function getImage()
+	{
+		return $this->upload->getImage();
+	}
 }
  
--- src/Walisph/Dashboard/Controllers/UploadController.php
+++ src/Walisph/Dashboard/Controllers/UploadController.php
@@ -1,30 +1,129 @@
 <?php namespace Walisph\Dashboard\Controllers;
 
 use Walisph\Dashboard\Traits\UploadTrait;
-use Illuminate\Filesystem\Filesystem;
 
 class UploadController extends DashboardController {
 
-    use UploadTrait;
+	use UploadTrait;
 
-    protected function createDirectory( $folder )
-    {
-        if( ! $this->dash->file->exists( public_path( $this->dash->config('walis-dashboard::upload.local') . $folder )) )
-        {
-            return $this->dash->file->makeDirectory( public_path( $this->dash->config('walis-dashboard::upload.local') . $folder ), 0777, true );
-        }
-        return true;
-    }
+	/**
+	 * @param $folder
+	 *
+	 * @return bool
+	 */
+	protected function createDirectory( $folder )
+	{
+		if( ! $this->dash->file->exists( public_path( $this->getConfiguration('upload', 'local') . $folder )) )
+		{
+			return $this->dash->file->makeDirectory( public_path( $this->getConfiguration('upload', 'local') . $folder ), 0777, true );
+		}
+		return true;
+	}
 
-    protected function compilingImage( $file, $directory )
-    {
-        $image = [];
-        $quality = [
-            'jpg' => $this->dash->config('walis-dashboard::upload.quality.jpg'),
-            'image' => $this->dash->config('walis-dashboard::upload.quality.image')
-        ];
-        $local_save_dir = public_path( $this->dash->config('walis-dashboard::upload.local') . $directory );
+	/**
+	 * @param $file
+	 * @param $directory
+	 *
+	 * @return array
+	 */
+	protected function compilingImage( $file, $directory )
+	{
+		$image = [];
+		$quality = [
+			'jpg' => $this->getConfiguration('upload', 'quality.jpg'),
+			'image' => $this->getConfiguration('upload', 'quality.image')
+		];
+		$local_save_dir = public_path( $this->getConfiguration('upload', 'local') . $directory );
+		$images = [
+			// SMALL size
+			$this->getImage()->make( $file )
+				->fit(
+				 $this->getConfiguration('upload', 'sizes.square.size' )
+				)
+				->encode(
+					'image/jpg',
+					$quality['jpg']
+				)
+				->save(
+					$local_save_dir . $this->createFileName( $this->getConfiguration('upload', 'sizes.square.name'), $file ),
+					$quality['image']
+				),
 
-    }
+			// THUMBNAIL size
+			$this->getImage()->make( $file )
+				->fit(
+					$this->getConfiguration('upload', 'sizes.thumbnail.size')
+				)
+				->encode(
+					'image/jpg',
+					$quality['jpg']
+				)
+				->save(
+					$local_save_dir . $this->createFileName( $this->getConfiguration('upload', 'sizes.thumbnail.name'), $file ),
+					$quality['image']
+				),
+
+			// SMALL size
+			$this->getImage()->make( $file )
+				->resize(
+					$this->getConfiguration('upload', 'sizes.small.width'),
+					null,
+					function( $constraint )
+					{
+						$constraint->aspectRatio();
+					}
+				)
+				->encode(
+					'image/jpg',
+					$quality['jpg']
+				)
+				->save(
+					$local_save_dir . $this->createFileName( $this->getConfiguration('upload', 'sizes.small.name'), $file ),
+					$quality['image']
+				),
+
+			// LARGE size
+			$this->getImage()->make( $file )
+				->resize(
+					$this->getConfiguration('upload', 'sizes.large.width'),
+					null,
+					function( $constraint )
+					{
+						$constraint->aspectRatio();
+					}
+				)
+				->encode(
+					'image/jpg',
+					$quality['jpg']
+				)
+				->save(
+					$local_save_dir . $this->createFileName( $this->getConfiguration('upload', 'sizes.large.name'), $file ),
+					$quality['image']
+				),
+
+		// ORIGINAL size
+		$this->getImage()->make( $file )
+			->encode(
+				'image/jpg',
+				$quality['jpg']
+			)
+			->save(
+				$local_save_dir . $this->createFileName( $this->getConfiguration('upload', 'sizes.original.name'), $file ),
+				$quality['image']
+			)
+
+		];
+
+		foreach( $images as $key => $value )
+		{
+			$image[] = [
+				'width' => $images[ $key ]->getWidth(),
+				'height' => $images[ $key ]->getHeight(),
+				'url' => $this->getConfiguration('upload', 'local') . "$directory$value->basename",
+				'base_dir' => "$directory$value->basename"
+			];
+		}
+		return $image;
+	}
 }
  
