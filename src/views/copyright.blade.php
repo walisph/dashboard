8976ccf3f77fdd5817fdf61225274258342f78b6
jayaregalinada <jayaregalinada@gmail.com>
2014-10-23 14:04:05 +0800

    Add the walls philippines copyright view

---
+++ src/views/copyright.blade.php
@@ -0,0 +1,34 @@
+<div id="walis_calling_card" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="myModalLabel">:)</h4>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid jumbotron">
+                    <h1 class="text-center">Thank you from Walis Philippines</h1>
+                    <div class="clearfix row">
+                        <div class="col-xs-4">
+                            {{ HTML::image('/assets/images/walisph/paramihan.png', 'paramihan', ['class' => 'img-100 img-responsive']) }}
+                        </div>
+                        <div class="col-xs-4">
+                            {{ HTML::image('/assets/images/walisph/ubl.png', 'ubl', ['class' => 'img-100 img-responsive']) }}
+                        </div>
+                        <div class="col-xs-4">
+                            {{ HTML::image('/assets/images/walisph/walis.png', 'walis', ['class' => 'img-100 img-responsive']) }}
+                        </div>
+                    </div>
+                    <hr />
+                    <p>
+                        If you want us to conceptualize, design and develop your very own website, contact the Walis Philippines at <a href="mailto:info@walis.com.ph" class="mail">info@walis.com.ph</a>. <br /><br />
+                        We would like to hear from you soon.
+                    </p>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <h1></h1>
+            </div>
+        </div>
+    </div>
+</div>
